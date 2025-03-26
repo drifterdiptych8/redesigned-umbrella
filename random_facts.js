a4ffba65354a3b0ddee7a90955b3188a8c63127f
William <drifter_diptych.8@icloud.com>
2025-03-26 21:47:27 +0100

    Create random_facts.js

---
+++ random_facts.js
@@ -0,0 +1,8 @@
+const facts = [
+    "🐙 An octopus has 3 hearts, but only 1 true love.",
+    "🌎 The Earth is actually 99% banana. Scientists just won’t admit it.",
+    "🦄 Unicorns existed, but they were invisible.",
+    "💾 If you whisper to a USB drive, it might store extra data."
+];
+
+console.log("📢 Fun fact: " + facts[Math.floor(Math.random() * facts.length)]);
